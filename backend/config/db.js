4415fa35f37461ceac284ef9fdf212f90a757693
Rukia <97436332+Rukia-D@users.noreply.github.com>
2024-08-13 23:49:21 +0530

    connection fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
    
 }
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
